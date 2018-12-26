4e162d4ac7aa2020926d5cf1d239a3d3c0f9e04a
FyuroTEX <yurilev4@gmail.com>
2018-12-26 14:35:58 +0200

    add market

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import { routes } from './routes';
+import { Link, Route } from 'react-router-dom';
+import { AdminPage } from './scenes/admin';
+import { products } from './data/products';
+
+const getProducts = async () => products;
+
+class App extends Component {
+  state = {
+    products: []
+  };
+
+ async  componentDidMount() { 
+    const prods = await getProducts();
+    this.setState({
+      products: prods,
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>
+          <Link to={routes.admin}>Admin</Link>
+        </p>
+        <Route
+          path={routes.admin}
+          render={renderProps => (
+            <AdminPage productList={this.state.products} {...renderProps} />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/products.js
@@ -0,0 +1,91 @@
+
+export const products = [
+  {
+    id: 1,
+    title: 'Apple iPhone 5 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 5000,
+  },
+  {
+    id: 2,
+    title: 'Apple iPhone 6 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 6000,
+  },
+  {
+    id: 3,
+    title: 'Apple iPhone 7 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 7000,
+  },
+  {
+    id: 4,
+    title: 'Apple iPhone 8 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 8000,
+  },
+  {
+    id: 5,
+    title: 'Apple iPhone 9 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 9000,
+  },
+  {
+    id: 6,
+    title: 'Apple iPhone 10 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 10000,
+  },
+  {
+    id: 7,
+    title: 'Apple iPhone 11 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 11000,
+  },
+  {
+    id: 8,
+    title: 'Apple iPhone 12 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 12000,
+  },
+  {
+    id: 9,
+    title: 'Apple iPhone 13 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 13000,
+  },
+  {
+    id: 10,
+    title: 'Apple iPhone 14 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 14000,
+  },
+  {
+    id: 11,
+    title: 'Apple iPhone 15 16Gb Black',
+    description:
+      'Екран Retina 4" (1136x640), ємнісний Multi-Touch / моноблок / двоядерний Apple A7 (1.3 ГГц) / ОЗП 1 ГБ / основна камера 8 Мп, фронтальна 1.2 Мп / Bluetooth 4.0 / Wi-Fi 802.11a/b/g/n / A-GPS / GLONASS / 32 ГБ вбудованої памяті / розєм 3.5 мм ',
+    image: 'https://hotline.ua/img/tx/862/862504_s265.jpg',
+    price: 15000,
+  },
+];
